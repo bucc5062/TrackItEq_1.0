f338bc736b3444eb1d391d8d756e0ad20ff1d091
justin hull <bucc5062@gmail.com>
2015-12-14 16:05:15 -0500

    modified GPS processing

--- app/src/main/java/com/newproject/jhull3341/trackiteq/gpsLocationListener.java
+++ app/src/main/java/com/newproject/jhull3341/trackiteq/gpsLocationListener.java
@@ -2,12 +2,16 @@ package com.newproject.jhull3341.trackiteq;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 
@@ -41,6 +45,7 @@ public class gpsLocationListener {
     private final static String eTAG = "Exception";
     private static final long MIN_TIME_BW_UPDATES = 1000;
     public gpsLocationListener(iGSPActivity main, Context me) {
+
         this.me = me;
         this.main = main;
         isRunning = true;
@@ -53,9 +58,7 @@ public class gpsLocationListener {
 
         if (hasFineLocationAccess != PackageManager.PERMISSION_GRANTED && hasCourseLocationAccess != PackageManager.PERMISSION_GRANTED) {
         }
-        //mlocManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME_BW_UPDATES, 0, mlocListener);
-
-        // GPS Position END
+        checkForProvider();
     }
 
     public void stopGPS() {
@@ -78,7 +81,47 @@ public class gpsLocationListener {
         mlocManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME_BW_UPDATES, 0, mlocListener);
         this.isRunning = true;
     }
+    private boolean checkForProvider() {
+        LocationManager lm = (LocationManager)me.getSystemService(Context.LOCATION_SERVICE);
+        boolean gps_enabled = false;
+        boolean network_enabled = false;
+        boolean rtnAnswer = true;
+        try {
+            gps_enabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        } catch(Exception ex) {}
 
+        try {
+            network_enabled = lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        } catch(Exception ex) {}
+
+        if(!gps_enabled && !network_enabled) {
+            rtnAnswer = true;
+            // notify user
+            AlertDialog.Builder dialog = new AlertDialog.Builder(me);
+            dialog.setMessage(me.getResources().getString(R.string.gps_network_not_enabled));
+            dialog.setPositiveButton(me.getResources().getString(R.string.open_location_settings), new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                    // TODO Auto-generated method stub
+                    Intent myIntent = new Intent( Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                    me.startActivity(myIntent);
+                    //get gps
+                }
+            });
+            dialog.setNegativeButton(this.me.getString(R.string.Cancel), new DialogInterface.OnClickListener() {
+
+                @Override
+                public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                    // TODO Auto-generated method stub
+
+                }
+            });
+            dialog.show();
+        }
+
+        return rtnAnswer;
+
+    }
     public class MyLocationListener implements LocationListener {
         @Override
         public void onLocationChanged(Location location) {
